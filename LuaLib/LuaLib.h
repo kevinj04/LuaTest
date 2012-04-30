c8ac9fb168ea6e6fce80b3cf46aaba72ea473d6c
Kevin Jenkins <kevinj04@gmail.com>
2012-04-30 15:17:14 -0400

    Initial Commit

---
+++ LuaLib/LuaLib.h
@@ -0,0 +1,13 @@
+//
+//  LuaLib.h
+//  LuaLib
+//
+//  Created by Kevin Jenkins on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LuaLib : NSObject
+
+@end
---
+++ LuaLib/LuaLib.m
@@ -0,0 +1,13 @@
+//
+//  LuaLib.m
+//  LuaLib
+//
+//  Created by Kevin Jenkins on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LuaLib.h"
+
+@implementation LuaLib
+
+@end
